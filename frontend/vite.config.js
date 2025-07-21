05098b6576316a396e77506159ee8edf6f0cdefa
Leela Satya SaiRam <214325905+LeelaSatyasairam@users.noreply.github.com>
2025-07-21 16:52:27 +0530

    addedbaseurl

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/wordle/', // 👈 this is crucial for Render deployment
   plugins: [react()],
 })
